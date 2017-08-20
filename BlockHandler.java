8fdb7ca35cc2ce5b567f8c5e4c8417eb0db29aa8
Matthew McAteer <matthewmcateer0@gmail.com>
2017-08-20 14:05:44 -0400

    Update BlockHandler.java

--- BlockHandler.java
+++ BlockHandler.java
@@ -1,26 +1,19 @@
-
 import java.security.PublicKey;
 
 public class BlockHandler {
     private BlockChain blockChain;
 
-    /** assume blockChain has the genesis block */
+    // assume blockChain has the genesis block
     public BlockHandler(BlockChain blockChain) {
         this.blockChain = blockChain;
     }
-
-    /**
-     * add {@code block} to the block chain if it is valid.
-     * 
-     * @return true if the block is valid and has been added, false otherwise
-     */
+    
     public boolean processBlock(Block block) {
         if (block == null)
             return false;
         return blockChain.addBlock(block);
     }
 
-    /** create a new {@code block} over the max height {@code block} */
     public Block createBlock(PublicKey myAddress) {
         Block parent = blockChain.getMaxHeightBlock();
         byte[] parentHash = parent.getHash();
@@ -40,7 +33,6 @@ public class BlockHandler {
             return null;
     }
 
-    /** process a {@code Transaction} */
     public void processTx(Transaction tx) {
         blockChain.addTransaction(tx);
     }
