2d886c00b96468d59ad2fd998cc0c3bca3e194b3
Matthew McAteer <matthewmcateer0@gmail.com>
2017-08-20 18:05:57 -0400

    Update Node.java

--- Node.java
+++ Node.java
@@ -3,19 +3,11 @@ import java.util.Set;
 
 public interface Node {
 
-    /** {@code followees[i]} is true if and only if this node follows node {@code i} */
     void setFollowees(boolean[] followees);
 
-    /** initialize proposal list of transactions */
     void setPendingTransaction(Set<Transaction> pendingTransactions);
 
-    /**
-     * @return proposals to send to my followers. REMEMBER: After final round, behavior of
-     *         {@code getProposals} changes and it should return the transactions upon which
-     *         consensus has been reached.
-     */
     Set<Transaction> sendToFollowers();
 
-    /** receive candidates from other nodes. */
     void receiveFromFollowees(Set<Candidate> candidates);
 }
